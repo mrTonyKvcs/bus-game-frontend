963c5ab724a3d962e02857988a9aab911eb31375
tony <attila.kovacs92@gmail.com>
2020-04-01 20:54:04 +0200

    playing with tailwinds

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,10 +1,11 @@
 <template>
-  <div>
+  <div class="container">
     <h1>Login</h1>
     <form @submit.prevent="login">
-      <input type="email" name="email" v-model="email">
-      <input type="password" name="password" v-model="password">
-      <button type="submit">Login</button>
+      <input class="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal" type="email" name="email" v-model="email" placeholder="jane@example.com">
+
+      <input class="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal" type="password" name="password" v-model="password" placeholder="jane@example.com">
+      <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Login</button>
     </form>
   </div>
 </template>
@@ -26,7 +27,7 @@ export default {
           password: this.password
         })
         .then(() => {
-          this.$router.push({ name: 'About' })
+          this.$router.push({ name: 'Dashboard' })
         })
         .catch(err => {
           console.log(err)
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,29 @@
 module.exports = {
   theme: {
-      backgroundColor: theme => ({
-        'primary': '#3490dc'
-      })
-  },
-  variants: {},
-  plugins: [],
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+    fontFamily: {
+      display: ['Gilroy', 'sans-serif'],
+      body: ['Graphik', 'sans-serif'],
+    },
+    borderWidth: {
+      default: '1px',
+      '0': '0',
+      '2': '2px',
+      '4': '4px',
+    },
+    extend: {
+      colors: {
+        cyan: '#9cdbff',
+      },
+      spacing: {
+        '96': '24rem',
+        '128': '32rem',
+      }
+    }
+  }
 }
