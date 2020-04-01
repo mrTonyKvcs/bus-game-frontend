d1c896119b6261b54cae478c7a97a63ebb2aa6b6
tony <attila.kovacs92@gmail.com>
2020-04-01 20:04:29 +0200

    set up authentication

--- src/App.vue
+++ src/App.vue
@@ -2,31 +2,30 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/dashboard" v-if="isLogged">Dashboard</router-link>
+      <router-link to="/login" v-else>Login</router-link>
+      <button type="button" @click="logout()" v-if="isLogged">
+        Logout
+      </button>
     </div>
     <router-view/>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import { mapGetters } from 'vuex'
 
-#nav {
-  padding: 30px;
-}
+export default {
+  computed: {
+    ...mapGetters([
+      'isLogged'
+    ])
+  },
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+  methods: {
+    logout () {
+      this.$store.dispatch('logout')
+    }
+  }
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -11,12 +11,17 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/dashboard',
+    name: 'Dashboard',
+    meta: {
+      auth: true
+    },
+    component: () => import(/* webpackChunkName: "about" */ '../views/Dashboard.vue')
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: () => import(/* webpackChunkName: "login" */ '../views/Login.vue')
   }
 ]
 
@@ -24,6 +29,16 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  const loggedIn = localStorage.getItem('user')
+
+  if (to.matched.some(record => record.meta.auth) && !loggedIn) {
+    next('/login')
+    return
+  }
+  next()
 })
 
 export default router
